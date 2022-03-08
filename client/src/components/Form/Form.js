08d25f81b91d0baa05259b1500ceb048e7d43fd1
Maksim Kokorin <importantonly696@gmail.com>
2022-03-08 08:17:17 +0200

    bugFix

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -50,7 +50,7 @@ export default function Form({ currentId, setCurrentId }) {
       <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
         <Typography variant='h6'>{currentId ? 'Editing' : 'Creating'} a Review</Typography>
         <TextField name='title' variant='outlined' label='Product' fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
-        <TextField name='message' variant='outlined' label='Review' fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
+        <TextField name='message' variant='outlined' label='Review' fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
         <TextField name='tags' variant='outlined' label='Tags' fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
         <div className={classes.fileInput}><FileBase type="file" multiple='false' onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
         <Button className={classes.buttonSubmit} variant='outlined' color='primary' size='large' type='submit' fullWidth>Submit</Button>
